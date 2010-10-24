3ad2aae53f6e9343f0319a2ef31b1fd0a3e367a5
Max Rydahl Andersen <max@jboss.org>
2010-10-24 22:22:40 +0200

    if no issues make tests green

--- testcase-render.py
+++ testcase-render.py
@@ -17,31 +17,35 @@ def render(self, logger, jira_env, args, results):
     doc = Document()
     testsuite = doc.createElement("testsuite")
     doc.appendChild(testsuite)
-    
-    #logger.info('Report \'' + reportName + '\', ' + str(len(results)) + ' issues')
-    #logger.info('%s\t%s\t%s' % ('Key', 'Assignee', 'Summary'))
-    for i, v in enumerate(results):
-        # For available field names, see the variables in
-        # src/java/com/atlassian/jira/rpc/soap/beans/RemoteIssue.java 
-        #logger.info('%s\t%s\t%s' % (v['key'], v['assignee'], v['summary']))
-        #  print v
-        fixVersion = ""
-        for version in v['fixVersions']:
-            fixVersion += '_' + version['name']
-                
-        testcase = doc.createElement("testcase")
-        testcase.setAttribute("classname",reportName)
-        testcase.setAttribute("name", v['key'] + xstr(fixVersion) + "_" + xstr(v['assignee']))
-        url = "https://jira.jboss.org/browse/" + v['key']
-            
-        error = doc.createElement("error")
-        error.setAttribute("message", desc + "( " + url + " )")
 
-        errortext = doc.createTextNode(v['summary'] + "(" + url + ")" )
-        error.appendChild(errortext)
-            
-        testcase.appendChild(error)
+    if len(results) > 0:
+        for i, v in enumerate(results):
+            # For available field names, see the variables in
+            # src/java/com/atlassian/jira/rpc/soap/beans/RemoteIssue.java 
+            #logger.info('%s\t%s\t%s' % (v['key'], v['assignee'], v['summary']))
+            #  print v
+            fixVersion = ""
+            for version in v['fixVersions']:
+                fixVersion += '_' + version['name']
+
+            testcase = doc.createElement("testcase")
+            testcase.setAttribute("classname",reportName)
+            testcase.setAttribute("name", v['key'] + xstr(fixVersion) + "_" + xstr(v['assignee']))
+            url = "https://jira.jboss.org/browse/" + v['key']
+
+            error = doc.createElement("error")
+            error.setAttribute("message", desc + "( " + url + " )")
+
+            errortext = doc.createTextNode(v['summary'] + "(" + url + ")" )
+            error.appendChild(errortext)
+
+            testcase.appendChild(error)
+            testsuite.appendChild(testcase)
+    else:
+        testcase = doc.createElement("testcase")
+        testcase.setAttribute("classname", reportName)
+        testcase.setAttribute("name", "noissues")
         testsuite.appendChild(testcase)
-        
+    
     print doc.toprettyxml(indent="  ")
     return 0    
