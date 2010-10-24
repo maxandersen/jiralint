1c62ffa008cf30edcd021a8cc1869814246a7fd4
Max Rydahl Andersen <max@jboss.org>
2010-10-24 20:09:41 +0200

    initial bulk

---
+++ jbosstools.sh
@@ -0,0 +1,7 @@
+#echo From The Build > $WORKSPACE/jlint-test.xml
+#cp ~/Documents/code/jlint/jira-cli-4.2/sample.xml sample-test.xml
+#echo FIXME: JBIDE-456 is broken because of bad fix. > tasks.txt
+#echo FIXME: https://jira.jboss.org/browse/JBDS-34 is even worse >> tasks.txt
+cd jira-cli-4.2/
+jira -s https://jira.jboss.org -u ${JIRA_USER} -p ${JIRA_PWD} report ToolsIllegalFixVersion > $WORKSPACE/jlint-test.xml
+
