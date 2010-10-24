bae7d2679ba9468d118195abbeefd2ac9f92704e
Max Rydahl Andersen <max@jboss.org>
2010-10-24 22:00:03 +0200

    documented usage

--- jbosstools.sh
+++ jbosstools.sh
@@ -3,5 +3,5 @@
 #echo FIXME: JBIDE-456 is broken because of bad fix. > tasks.txt
 #echo FIXME: https://jira.jboss.org/browse/JBDS-34 is even worse >> tasks.txt
 cd jira-cli-4.2/
-./jira -s https://jira.jboss.org -u ${JIRA_USER} -p ${JIRA_PWD} report ToolsIllegalFixVersion > $WORKSPACE/jlint-test.xml
+./jira -s https://jira.jboss.org -u ${JIRA_USER} -p ${JIRA_PWD} -r ../testcase-render.py report ToolsIllegalFixVersion > $WORKSPACE/jlint-test.xml
 
