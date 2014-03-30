7d0d6c96fd0cc3f6cc0386e1bacde4bd78375564
Max Rydahl Andersen <max@jboss.org>
2014-03-30 09:15:27 +0200

    cleanup repo now we do not need external client library

--- jbosstools.sh
+++
@@ -1,8 +0,0 @@
-#echo From The Build > $WORKSPACE/jlint-test.xml
-#cp ~/Documents/code/jlint/jira-cli-4.2/sample.xml sample-test.xml
-#echo FIXME: JBIDE-456 is broken because of bad fix. > tasks.txt
-#echo FIXME: https://jira.jboss.org/browse/JBDS-34 is even worse >> tasks.txt
-cd jira-cli-4.2/
-./jira -s https://jira.jboss.org -u ${JIRA_USER} -p ${JIRA_PWD} -r ../testcase-render.py report ToolsIllegalFixVersion > $WORKSPACE/jlint-test.xml
-./jira -s https://jira.jboss.org -u ${JIRA_USER} -p ${JIRA_PWD} -r ../testcase-render.py report ToolsUnresolvedNoComponent > $WORKSPACE/missingcomponent-test.xml
-./jira -s https://jira.jboss.org/jira logout
