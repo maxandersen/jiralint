294a049b5698d8ab3704ad1cc46082f29777bd9c
Max Rydahl Andersen <manderse@redhat.com>
2014-12-19 00:37:31 +0100

    add support for constants.json to define variables that can be referred in jql

--- setup_filters.py
+++ setup_filters.py
@@ -1,12 +1,29 @@
 from optparse import OptionParser
-from common import shared 
+from common import shared
+import re
 import json
+import sys
 
 def saveFilters(name, filters):
     with open(name,'w') as outfile:
         json.dump(filters, outfile,indent=4, sort_keys=True)
 
-        
+
+def listVersions(project, pattern):
+    versions = shared.jiraquery(options,"/rest/api/latest/project/" + project + "/versions")
+    #versionmatch = re.compile('4.2.(?!x).*')
+    versionmatch = re.compile(pattern)
+    foundversions = []
+    for version in versions:
+       # print version['name']
+        if versionmatch.match(version['name']):
+            foundversions.append('"' + version['name'] + '"')
+
+    result = ", ".join(foundversions)
+
+    
+    return result
+
 usage = "usage: %prog -u <user> -p <password> -f <filters.json>\nCreate/maintain set of filters defined in filters.json."
 
 parser = OptionParser(usage)
@@ -23,9 +40,18 @@ if not options.username or not options.password:
     parser.error("Missing username or password")
 
 if options.filterfile:
-    print "Force enabling global shared filters. Will not have any effect if user is not allowed to globally share objects in jira."
-    shared.jiraupdate(options, "/rest/api/latest/filter/defaultShareScope", { 'scope': 'GLOBAL' })
+    #print "Force enabling global shared filters. Will not have any effect if user is not allowed to globally share objects in jira."
+    #shared.jiraupdate(options, "/rest/api/latest/filter/defaultShareScope", { 'scope': 'GLOBAL' })
 
+    print "Load constants"
+    constantdef = json.load(open("constants.json", 'r'))
+    constants = {}
+    for name, fields in constantdef.items():
+        method = getattr(sys.modules[__name__], fields['function'])
+        del fields['function']
+        constants[name] = method(**fields)
+        print name + "->" + constants[name]
+    
     print "Using filters defined in " + options.filterfile
     filters = json.load(open(options.filterfile, 'r'))
 
@@ -34,12 +60,12 @@ if options.filterfile:
         data = {
                 'name': name,
                 'description': fields['description'],
-                'jql': fields['jql'],
+                'jql': fields['jql'] % constants,
                 'favourite' : 'true'
         }
+        
         if 'id' in fields:
             print 'updating ' + name
-            data['name'] = data['name']
             fields['id'] = shared.jiraupdate(options, "/rest/api/latest/filter/" + fields['id'], data)['id']
         else:
             print 'creating ' + name
